af71b677bcae84241620b6272f7ac2ef9c1a2af9
s4mper <contactos4mper@gmail.com>
2024-05-28 23:52:31 +0200

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -30,7 +30,7 @@ const CONFIG = {
 
   // notion configuration (required)
   notionConfig: {
-    pageId: process.env.b236b9b3da9648d59f9901eba0f77dea,
+    pageId: process.env.NOTION_PAGE_ID
   },
 
   // plugin configuration (optional)
